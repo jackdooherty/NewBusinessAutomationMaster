382568a273d2477c70c197cc90157b511d41479f
jdoher <jack.doherty@nucleusfinancial.com>
2022-04-13 13:00:18 +0100

    Test runner update

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -4,8 +4,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        //features = "src/test/resources",
-        features = "src/test/resources/IndividualCreate.feature",
+        features = "src/test/resources",
         //features = "src/test/resources/Login.feature",
         glue = "StepDefinitions",
         plugin = {"pretty", "html:target/cucumber-reports/cucumber.html",
